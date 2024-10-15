21a3d5228000b82d6eaf37b98a7007603a9b97c1
Georgina Serra <georginaserraoliveras@gmail.com>
2024-10-15 16:54:46 +0200

    Clean up grid

--- src/GanxoTapesGrid.tsx
+++ src/GanxoTapesGrid.tsx
@@ -2,10 +2,10 @@ import { IGanxoTapa } from "./api";
 import GanxoTapesGridItem from "./GanxoTapesGridItem";
 
 const GanxoTapesGrid = ({ ganxotapes }: { ganxotapes: IGanxoTapa[] }) => (
-  <div className="mx-auto max-w-7xl px-4 md:px-6 lg:px-8">
+  <div className="mx-auto max-w-7xl px-4 py-8 md:px-6 lg:px-8">
     <ul
       role="list"
-      className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+      className="grid sm:grid-cols-2 gap-x-4 gap-y-8 md:grid-cols-3 md:gap-x-6 lg:grid-cols-4 lg:gap-x-8"
     >
       {ganxotapes.map((ganxotapa) => (
         <GanxoTapesGridItem key={ganxotapa.id} info={ganxotapa} />
@@ -14,61 +14,4 @@ const GanxoTapesGrid = ({ ganxotapes }: { ganxotapes: IGanxoTapa[] }) => (
   </div>
 );
 
-// export default GanxoTapesGrid;
-
-// absolute pt-14 bg-gradient-to-t from-purple-500
-const Item = ({ info }: { info: IGanxoTapa }) => {
-  //   return (
-  //     <li className="relative">
-  //       <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24">
-  //         <img
-  //           src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a"
-  //           alt="University of Southern California"
-  //           className="absolute inset-0 h-full w-full object-cover"
-  //         />
-  //         <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-  //         <h3 className="z-10 mt-3 text-3xl font-bold text-white">Paris</h3>
-  //         <div className="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-  //           City of love
-  //         </div>
-  //       </article>
-  //     </li>
-  //   );
-  return (
-    <li className="relative">
-      <img
-        alt=""
-        src={`https://www.visitguixols.com/images/${info.img}.jpg`}
-        //   className="u arb btn"
-        className="rounded-md object-cover h-48 w-full"
-      />
-      {/* <p className="font-medium">{info.restaurant}</p> */}
-      <div className="absolute inset-x-0 top-0 p-4 rounded-md text-white font-medium bg-gradient-to-b from-gray-900">
-        {info.restaurant}
-      </div>
-      <span className="flex flex-col gap-2">
-        <h3 className="mt-6 text-sm font-medium text-gray-900 flex-1">
-          {info.type === "tapa" && <div>{info.title}</div>}
-          {info.type === "menu" && (
-            <div className="whitespace-pre-line">{info.menu}</div>
-          )}
-        </h3>
-        <dd className="text-sm text-gray-600">{info.schedule2}</dd>
-      </span>
-    </li>
-  );
-};
-const ItemGrid = ({ ganxotapes }: { ganxotapes: IGanxoTapa[] }) => (
-  <div className="mx-auto max-w-7xl px-4 py-8 md:px-6 lg:px-8">
-    <ul
-      role="list"
-      className="grid sm:grid-cols-2 gap-x-4 gap-y-8 md:grid-cols-3 md:gap-x-6 lg:grid-cols-4 lg:gap-x-8"
-    >
-      {ganxotapes.map((ganxotapa) => (
-        <Item key={ganxotapa.id} info={ganxotapa} />
-      ))}
-    </ul>
-  </div>
-);
-
-export default ItemGrid;
+export default GanxoTapesGrid;
--- src/GanxoTapesGridItem.tsx
+++ src/GanxoTapesGridItem.tsx
@@ -2,79 +2,25 @@ import { IGanxoTapa } from "./api";
 
 const GanxoTapesGridItem = ({ info }: { info: IGanxoTapa }) => {
   return (
-    <li className="col-span-1 flex flex-col acr acv aem ami text-center bcn">
-      <div className="flex flex-1 flex-col p-8">
-        <img
-          src={`https://www.visitguixols.com/images/${info.img}.jpg`}
-          alt="Ganxotapa imatge"
-          className="rounded-md"
-        />
-        <h3 className="mt-6 text-sm font-medium text-gray-900">
-          {info.type === "tapa" && <div>{info.title}</div>}
-          {info.type === "menu" && (
-            <div className="whitespace-pre-line">{info.menu}</div>
-          )}
-        </h3>
-        <dl className="mt-1 flex grow flex-col justify-between">
-          <dt className="sr-only">Horaris</dt>
-          <dd className="text-sm text-gray-400">{info.schedule2}</dd>
-          <dt className="sr-only">Restaurant</dt>
-          <dd className="mt-3">
-            <span className="inline-flex items-center rounded-full akb asa asm axf font-medium azd bda bde bdz">
-              {info.restaurant}
-            </span>
-          </dd>
-        </dl>
-      </div>
-    </li>
-  );
-  return (
-    <li className="col-span-1 flex flex-col acr acv aem ami text-center bcn">
-      <div className="flex flex-1 flex-col p-8">
-        <img
-          src={`https://www.visitguixols.com/images/${info.img}.jpg`}
-          alt="Ganxotapa imatge"
-          className="mx-auto h-32 w-32 shrink-0 rounded-full object-cover"
-        />
-        <h3 className="mt-6 text-sm font-medium text-gray-900">
-          {info.type === "tapa" && <div>{info.title}</div>}
-          {info.type === "menu" && (
-            <div className="whitespace-pre-line">{info.menu}</div>
-          )}
-        </h3>
-        <dl className="mt-1 flex grow flex-col justify-between">
-          <dt className="sr-only">Horaris</dt>
-          <dd className="text-sm text-gray-400">{info.schedule2}</dd>
-          <dt className="sr-only">Restaurant</dt>
-          <dd className="mt-3">
-            <span className="inline-flex items-center rounded-full akb asa asm axf font-medium azd bda bde bdz">
-              {info.restaurant}
-            </span>
-          </dd>
-        </dl>
-      </div>
-    </li>
-  );
-  return (
-    <div className="grid auto-rows-fr">
-      <div className="">{info.restaurant}</div>
-      <div className="">
-        {info.type === "tapa" && (
-          <div className="text-gray-600">{info.title}</div>
-        )}
-        {info.type === "menu" && (
-          <div className="whitespace-pre-line">{info.menu}</div>
-        )}
-      </div>
+    <li className="relative">
       <img
+        alt=""
         src={`https://www.visitguixols.com/images/${info.img}.jpg`}
-        alt="Ganxotapa imatge"
-        width={200}
-        height={200}
-        className=""
+        className="rounded-md object-cover h-48 w-full"
       />
-      <div className="">{info.schedule2}</div>
-    </div>
+      <div className="absolute inset-x-0 top-0 p-4 rounded-md text-white font-medium bg-gradient-to-b from-gray-900">
+        {info.restaurant}
+      </div>
+      <span className="flex flex-col gap-2">
+        <h3 className="mt-6 text-sm font-medium text-gray-900 flex-1">
+          {info.type === "tapa" && <div>{info.title}</div>}
+          {info.type === "menu" && (
+            <div className="whitespace-pre-line">{info.menu}</div>
+          )}
+        </h3>
+        <dd className="text-sm text-gray-600">{info.schedule2}</dd>
+      </span>
+    </li>
   );
 };
 
